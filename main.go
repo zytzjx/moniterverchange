9ba1d5419e0ac2dfd604d5ebaf5faf619885c55a
Jeffery <zytzjx@sina.com>
2022-09-13 11:27:01 -0700

    use iTunes update

--- main.go
+++ main.go
@@ -17,6 +17,26 @@ import (
 	gomail "gopkg.in/mail.v2"
 )
 
+type Package struct {
+	Size int
+	URL  string
+}
+
+type Product struct {
+	ServerMetadataURL string
+	Packages          []Package
+	PostDate          time.Time
+	Distributions     map[string]string
+}
+
+type CataLogs struct {
+	CatalogVersion int `plist:"CatalogVersion"`
+	ApplePostURL   string
+	ApplePostFreq  string
+	IndexDate      time.Time
+	Products       map[string]Product
+}
+
 func SendReport(v1, v2 string) {
 	m := gomail.NewMessage()
 	from := os.Getenv("frommail")
@@ -48,6 +68,36 @@ func SendReport(v1, v2 string) {
 
 }
 
+func iTunesUpdate() {
+	appleurl := "https://swcatalog.apple.com/content/catalogs/others/index-windows-1.sucatalog"
+	client := resty.New()
+	resp, err := client.R().Get(appleurl)
+	if err != nil {
+		color.Redf("error: %v\n", err)
+		return
+	}
+	var data CataLogs
+	buf := bytes.NewReader(resp.Body())
+	decoder := plist.NewDecoder(buf)
+	err = decoder.Decode(&data)
+	//_, err = plist.Unmarshal(resp.Body(), data)
+	if err != nil {
+		color.Redf("error: %v\n", err)
+		return
+	}
+	color.Greenf("%v\n", data)
+	vd := os.Getenv("versiondate")
+	layout := "2006-01-02T15:04:05Z"
+	t, err := time.Parse(layout, vd)
+	if err != nil {
+		color.Println("config file versiondate format error, example: " + layout)
+		return
+	}
+	if data.IndexDate.Sub(t).Hours() > 24.0 {
+		SendReport(vd, data.IndexDate.Format(layout))
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -60,6 +110,7 @@ func main() {
 
 	s := gocron.NewScheduler(time.UTC)
 	s.Every(1).Day().At("7:30").Do(func() {
+		iTunesUpdate()
 		client := resty.New()
 		resp, err := client.R().Get("https://s.mzstatic.com/version?machineID=c2d9cd28d8c9349c")
 		if err != nil {
